346c8c3ad5081445e3388fd0e8b0f619803bad63
JeSnp <jeff.soliveira15@gmail.com>
2026-01-15 18:02:36 -0300

    Adiciona base de testes de API e cenários de login com RestAssured

--- src/test/java/com/exemplo/selenium/api/BaseApiTest.java
+++ src/test/java/com/exemplo/selenium/api/BaseApiTest.java
@@ -1,15 +1,81 @@
 package com.exemplo.selenium.api;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
 import io.restassured.specification.RequestSpecification;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class BaseApiTest {
 
+    static WireMockServer wireMockServer;
+
+    @BeforeAll
+    public static void setup() {
+        // 🔹 Inicia o mock server
+        wireMockServer = new WireMockServer(8080);
+        System.setProperty("file.encoding", "UTF-8");
+        wireMockServer.start();
+
+        // 🔹 Configura o RestAssured
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8080;
+        RestAssured.defaultParser = Parser.JSON;
+
+        // 🔹 Configura o WireMock
+        configureFor("localhost", 8080);
+
+        // ----------------------------------------------------------
+        // MOCKS DE LOGIN
+        // ----------------------------------------------------------
+
+        // 1️⃣ Login válido
+        stubFor(post(urlEqualTo("/login"))
+            .withRequestBody(matchingJsonPath("$.username", equalTo("user")))
+            .withRequestBody(matchingJsonPath("$.password", equalTo("123456")))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json; charset=UTF-8")
+                .withStatus(200)
+                .withBody("{\"token\":\"abc123\", \"perfil\":\"ADMIN\"}")));
+
+        // 2️⃣ Credenciais inválidas
+        stubFor(post(urlEqualTo("/login"))
+            .withRequestBody(matchingJsonPath("$.password", equalTo("senha_errada")))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json; charset=UTF-8")
+                .withStatus(401)
+                .withBody("{\"mensagem\":\"Usuário ou senha incorretos\"}")));
+
+        // 3️⃣ Usuário sem permissão
+        stubFor(post(urlEqualTo("/login"))
+            .withRequestBody(matchingJsonPath("$.username", equalTo("user_sem_permissao")))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json; charset=UTF-8")
+                .withStatus(403)
+                .withBody("{\"mensagem\":\"sem permissão\"}")));
+
+        // 4️⃣ Usuário bloqueado
+        stubFor(post(urlEqualTo("/login"))
+            .withRequestBody(matchingJsonPath("$.username", equalTo("user_bloqueado")))
+            .willReturn(aResponse()
+                .withHeader("Content-Type", "application/json; charset=UTF-8")
+                .withStatus(423)
+                .withBody("{\"mensagem\":\"bloqueado\"}")));
+    }
+
+    @AfterAll
+    public static void teardown() {
+        if (wireMockServer != null && wireMockServer.isRunning()) {
+            wireMockServer.stop();
+        }
+    }
+
     protected RequestSpecification request() {
         return RestAssured
-                .given()
-                .baseUri("https://google.com") 
-                .basePath("/api")
-                .contentType("application/json");
+            .given()
+            .contentType("application/json");
     }
 }
--- src/test/java/com/exemplo/selenium/api/LoginApiTest.java
+++ src/test/java/com/exemplo/selenium/api/LoginApiTest.java
@@ -1,62 +1,57 @@
 package com.exemplo.selenium.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+
 
 public class LoginApiTest extends BaseApiTest {
 
-    @Test
-    public void deveRetornar200ComTokenEPerfil() {
-        String body = "{ \"username\": \"user\", \"password\": \"123456\" }";
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("cenariosDeLogin")
+    void deveTestarCenariosDeLogin(
+            String descricao,
+            String username,
+            String password,
+            int statusEsperado,
+            String mensagemEsperada
+    ) {
 
+        String body = String.format(
+            "{ \"username\": \"%s\", \"password\": \"%s\" }",
+            username,
+            password
+        );
 
-        request()
-            .body(body)
-        .when()
-            .post("/login")
-        .then()
-            .statusCode(200)
-            .body("token", notNullValue())
-            .body("perfil", anyOf(equalTo("ADMIN"), equalTo("USER")));
+        io.restassured.response.ValidatableResponse response =
+            request()
+                .body(body)
+            .when()
+                .post("/login")
+            .then()
+                .statusCode(statusEsperado);
+
+        if (statusEsperado == 200) {
+            response
+                .body("token", notNullValue())
+                .body("perfil", anyOf(equalTo("ADMIN"), equalTo("USER")));
+        } else {
+            response
+                .body("mensagem", containsString(mensagemEsperada));
+        }
     }
 
-    @Test
-    public void deveRetornar401ComCredenciaisInvalidas() {
-        String body = "{ \"username\": \"user\", \"password\": \"123456\" }";
-;
-
-        request()
-            .body(body)
-        .when()
-            .post("/login")
-        .then()
-            .statusCode(401)
-            .body("mensagem", containsString("Usuário ou senha incorretos"));
-    }
-
-    @Test
-    public void deveRetornar403ParaAcessoNegado() {
-        String body = "{ \"username\": \"user\", \"password\": \"123456\" }";
-
-        request()
-            .body(body)
-        .when()
-            .post("/login")
-        .then()
-            .statusCode(403)
-            .body("mensagem", containsString("sem permissão"));
-    }
-
-    @Test
-    public void deveRetornar423ParaUsuarioBloqueado() {
-        String body = "{ \"username\": \"user\", \"password\": \"123456\" }";
-
-        request()
-            .body(body)
-        .when()
-            .post("/login")
-        .then()
-            .statusCode(423)
-            .body("mensagem", containsString("bloqueado"));
+    static Stream<org.junit.jupiter.params.provider.Arguments> cenariosDeLogin() {
+        return Stream.of(
+            arguments("Login válido", "user", "123456", 200, null),
+            arguments("Credenciais inválidas", "user", "senha_errada", 401, "Usuário ou senha incorretos"),
+            arguments("Usuário sem permissão", "user_sem_permissao", "123456", 403, "sem permissão"),
+            arguments("Usuário bloqueado", "user_bloqueado", "123456", 423, "bloqueado")
+        );
     }
 }
